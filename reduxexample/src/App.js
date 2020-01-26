aa1edd4da317f0fd00c5228f180a390ab51d3b22
Thiago Vieira <th91v@outlook.com>
2020-01-26 12:52:52 -0300

    estrutura inicial do projeto

---
+++ reduxexample/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Posts from './components/Posts';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <Posts />
+    </div>
+  );
+}
+
+export default App;
