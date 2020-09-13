0653487281387606918130a720b27acbd7ec3769
Thiago Vieira <th91v@outlook.com>
2020-09-12 22:07:07 -0300

    refactor: new beginning

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import Posts from './components/Posts';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -20,7 +19,6 @@ function App() {
           Learn React
         </a>
       </header>
-      <Posts />
     </div>
   );
 }
